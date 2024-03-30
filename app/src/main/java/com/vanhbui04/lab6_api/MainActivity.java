06461620c260f9faba1cba48d86865fb5589f946
avnhbui <vanhbui04@gmail.com>
2024-03-30 22:01:55 +0700

    first commit

---
+++ app/src/main/java/com/vanhbui04/lab6_api/MainActivity.java
@@ -0,0 +1,30 @@
+package com.vanhbui04.lab6_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView view;
+    Button getdata;
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = findViewById(R.id.textview);
+        getdata = findViewById(R.id.getdata);
+        getdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VolleyFN volleyFN = new VolleyFN();
+                volleyFN.getAllDataFromAPI(context,view);
+            }
+        });
+    }
+}
